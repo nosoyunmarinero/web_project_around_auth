b6f71d3a3d4ea33e68cab7dc6e2dd064a46251d9
FranciscoRV <nosoyunmarinero@gmail.com>
2025-07-23 17:17:01 -0600

    Se agrego la validacion correcta de form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,29 +1,24 @@
-import { useState, useRef,useEffect} from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Header from "../Header/Header";
 import formValidator from '../../utils/FormValidator';
-import InfoTooltipSucess from '../InfoTooltip/InfoTooltipSucess';
 
-
-function Login(props) {
+function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const inputRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
   const formRef = useRef();
 
-  const { handleOpenPopup } = props;
-
-  const infoToolpopup = { title: "", children: <InfoTooltipSucess /> };
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
+  }
 
   useEffect(() => {
     if (formRef.current) {
       formValidator.setForm(formRef.current).enableValidation();
     }
   }, []);
-
-  function handleSubmit(e) {
-  e.preventDefault();
-  handleOpenPopup();
-}
 
   return (
     <div className="login">
@@ -35,33 +30,30 @@ function Login(props) {
       </h1>
       <form className="login_form" onSubmit={handleSubmit} id="login-form" noValidate ref={formRef}>
         <input type="email"
-        id="email"
-        ref={inputRef}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="profile__edit-form-input login_input"
-        placeholder="Correo electrónico"
-        minLength="2"
-        maxLength="40"
-        required
+          id="email"
+          ref={emailRef}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="profile__edit-form-input login_input"
+          placeholder="Correo electrónico"
+          minLength="2"
+          maxLength="40"
+          required
         />
         <span className="form__input-error" id="email-error"></span>
         <input type="password"
-        id="password"
-
-        ref={inputRef}
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        className="profile__edit-form-input login_input"
-        placeholder="Contraseña"
-        minLength="2"
-        maxLength="40"
-        required
+          id="password"
+          ref={passwordRef}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="profile__edit-form-input login_input"
+          placeholder="Contraseña"
+          minLength="2"
+          maxLength="40"
+          required
         />
         <span className="form__input-error" id="password-error"></span>
-        <button type="submit" className="login_submit" onClick={() => {
-         handleOpenPopup(infoToolpopup)
-        }}>Inicia sesión</button>
+        <button type="submit" className="profile__edit-form-button login_submit">Inicia sesión</button>
       </form>
       <button className="signup_button">¿Aún no eres miembro? Regístrate aquí</button>
     </div>
